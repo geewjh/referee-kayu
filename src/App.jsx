57f8b8414f534194477928b9dec7adf3a80b30eb
geewjh <geewjh@gmail.com>
2024-03-22 21:15:24 +0800

    try fetch schedules

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState, useEffect } from "react";
-import NavBar from "./components/NavBar";
+import NavBar from "./components/NavBar/NavBar";
 import { fetchStandings } from "./components/Standings/fetchStandings";
 import { Routes, Route } from "react-router-dom";
 import Standings from "./components/Standings/Standings";
+import { fetchSchedulesByLeague } from "./components/Scheduled/fetchSchedulesByLeague";
 
 function App() {
   const [standings, setStandings] = useState([]);
+  const [schedules, setSchedules] = useState(null);
 
   useEffect(() => {
     const fetchStandingsData = async () => {
@@ -18,6 +20,20 @@ function App() {
     };
     fetchStandingsData();
   }, []);
+
+  useEffect(() => {
+    const fetchSchedulesData = async () => {
+      try {
+        const schedulesData = await fetchSchedulesByLeague();
+        setSchedules(schedulesData);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchSchedulesData();
+  }, []);
+
+  console.log(schedules);
 
   return (
     <div>
---
+++ src/components/Scheduled/fetchSchedulesByLeague.js
@@ -0,0 +1,20 @@
+export const fetchSchedulesByLeague = async () => {
+  const apiKey = import.meta.env.VITE_API_KEY;
+  const url =
+    "https://api-football-v1.p.rapidapi.com/v3/fixtures?league=39&season=2023";
+  const options = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": apiKey,
+      "X-RapidAPI-Host": "api-football-v1.p.rapidapi.com",
+    },
+  };
+
+  try {
+    const response = await fetch(url, options);
+    const result = await response.json();
+    return result;
+  } catch (error) {
+    console.error(error);
+  }
+};
